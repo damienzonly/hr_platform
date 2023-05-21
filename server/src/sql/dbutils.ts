1db3507215eaaf8d6e214763bf681407362d9a98
damienzonly <damianomails@gmail.com>
2023-05-21 22:20:45 +0200

    sprcifying schema to connecto to

--- server/src/sql/dbutils.ts
+++ server/src/sql/dbutils.ts
@@ -4,6 +4,7 @@ import { QueryTypes, Sequelize } from "sequelize";
 export const db = new Sequelize({
     database: 'postgres',
     dialect: 'postgres',
+    schema: 'hr_platform',
     host: process.env.DB_HOST as any,
     port: process.env.DB_PORT as any,
     username: process.env.DB_USER as any,
