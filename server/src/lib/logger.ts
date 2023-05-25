0743511133109158f8276d80f0eb9dd413b40af2
damienzonly <damianomails@gmail.com>
2023-05-26 00:49:13 +0200

    removed logger meta

--- server/src/lib/logger.ts
+++ server/src/lib/logger.ts
@@ -2,7 +2,6 @@ import winston from 'winston'
 
 export const logger = winston.createLogger({
     level: process.env.LOG_LEVEL || 'debug',
-    defaultMeta: { service: 'hr-server' },
     // format: winston.format.timestamp(),
     transports: [new winston.transports.Console()]
 })
