5178e817075917886f30cf38806852c77d9ce81d
damienzonly <damianomails@gmail.com>
2023-05-26 01:30:23 +0200

    saved test initsql

--- initdb/init.sql
+++ initdb/init.sql
@@ -21,7 +21,7 @@ create table if not exists hr_platform.billable_entity (
 
 create table if not exists hr_platform.consultant (
     id serial primary key,
-    billable_entity_id integer not null references hr_platform.billable_entity (id), -- must own a parent billable entity
+    billable_entity_id integer not null,
     name varchar not null,
     surname varchar not null,
     experience_years integer not null,
@@ -32,12 +32,12 @@ create table if not exists hr_platform.consultant (
 create table if not exists hr_platform.client (
     id serial primary key,
     company_name varchar not null,
-    billable_entity_id integer not null references hr_platform.billable_entity (id)
+    billable_entity_id integer not null
 );
 
 create table if not exists hr_platform.commission (
     id serial primary key,
-    client_id integer not null references hr_platform.client (id),
+    client_id integer not null,
     budget integer not null,
     start_date timestamp not null,
     duration integer not null
@@ -45,9 +45,9 @@ create table if not exists hr_platform.commission (
 
 create table if not exists hr_platform.bills (
     id serial primary key,
-    source_billable_id integer not null references hr_platform.billable_entity (id),
-    destination_billable_id integer not null references hr_platform.billable_entity (id),
-    commission_id integer not null references hr_platform.commission (id),
+    source_billable_id integer not null,
+    destination_billable_id integer not null,
+    commission_id integer not null,
     bill_identifier_id integer not null,
     payed_date timestamp,
     iban varchar not null,
@@ -56,14 +56,14 @@ create table if not exists hr_platform.bills (
 
 create table if not exists hr_platform.team_of_commission (
     id serial primary key,
-    commission_id integer not null references hr_platform.commission (id),
-    consultant_id integer not null references hr_platform.consultant (id),
+    commission_id integer not null,
+    consultant_id integer not null,
     start_date timestamp not null
 );
 
 create table if not exists hr_platform.cv_job_history (
     id serial primary key,
-    consultant_id integer not null references hr_platform.consultant(id),
+    consultant_id integer not null,
     from_date timestamp not null,
     to_date timestamp not null,
     company_name varchar not null,
@@ -72,21 +72,21 @@ create table if not exists hr_platform.cv_job_history (
 
 create table if not exists hr_platform.cv_skills (
     id serial primary key,
-    consultant_id integer not null references hr_platform.consultant(id),
-    skill_id integer not null references hr_platform.skill (id),
+    consultant_id integer not null,
+    skill_id integer not null,
     year_of_experience integer not null
 );
 
 create table if not exists hr_platform.job_spec_requirements (
     id serial primary key,
-    skill_id integer not null references hr_platform.skill (id),
-    commission_id integer not null references hr_platform.commission (id),
+    skill_id integer not null,
+    commission_id integer not null,
     years_of_experience integer not null
 );
 
 create table if not exists hr_platform.hiring_details (
     id serial primary key,
-    consultant_id integer not null references hr_platform.consultant (id),
-    hr_employee_id integer not null references hr_platform.hr_employee (id),
+    consultant_id integer not null,
+    hr_employee_id integer not null,
     start_date timestamp not null
 );
