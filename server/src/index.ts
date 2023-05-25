d2f5618e08847ef22e9986b8fee9d0e574236d1d
damienzonly <damianomails@gmail.com>
2023-05-26 01:06:15 +0200

    crud all entities + db retry

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,7 +8,22 @@ import escape from 'sql-escape-string'
 
 const port = process.env.HTTP_PORT || 5000;
 
-function creator(item, tableName: string) {
+const entities = [
+    'hr_employee',
+    'consultant',
+    'billable_entity',
+    'skill',
+    'bills',
+    'client',
+    'cv_skills',
+    'commission',
+    'team_of_commission',
+    'job_spec_requirements',
+    'cv_job_history',
+    'hiring_details'
+]
+
+async function creator(item, tableName: string) {
     if (!_.isNil(item?.id)) delete item.id;
 
     // sort item keys for insertion query order
@@ -25,7 +40,8 @@ function creator(item, tableName: string) {
         return value
     })
 
-    return rawquery(`insert into ${tableName} (${columns}) VALUES (${values}) returning id`, QueryTypes.INSERT)
+    const [[data]] = await rawquery(`insert into ${tableName} (${columns}) VALUES (${values}) returning id`, QueryTypes.INSERT)
+    return data;
 }
 
 function deleter(id, tableName: string) {
@@ -36,9 +52,11 @@ async function getter(sql) {
     return rawquery(sql, QueryTypes.SELECT) as any
 }
 
-function initCrud(app, entity: string, tableName) {
+function initCrud(app, entity: string, tableName = entity) {
+    tableName = `hr_platform.${entity}`
+    logger.info(`initializing crud for ${entity}`);
     makeCrud(app, entity, {
-        creator: (req) => creator(req.body.item, tableName),
+        creator: (req) => creator(req.body, tableName),
         deleter: req => deleter(req.params.id, tableName),
         getter: req => getter(`select * from ${tableName} where id = ${req.params.id}`), // todo
         lister: () => getter(`select * from ${tableName}`), // todo
@@ -47,18 +65,31 @@ function initCrud(app, entity: string, tableName) {
 }
 
 ;(async () => {
-    await db.authenticate();
+    while(1) {
+        try {
+            await db.authenticate();
+            break
+        } catch (e) {
+            await new Promise(r => setTimeout(r, 1000))
+        }
+    }
 
-    const entities = [
-        ["clients", "client"],
-        ["consultant"],
-        ["hr_employee", "hr_employee"],
-        ["billable_entity"],
-        ["bills"],
-        ["commission"],
-        ["skill"],
-    ]
+    for (const entity of entities) {
+        initCrud(app, entity)
+    }
 
-    entities.forEach(e => initCrud(app, e[0], `hr_platform.${e[1] || e[0]}`))
+    // const entities = [
+    //     ["clients", "client"],
+    //     ["consultant"],
+    //     ["hr_employee", "hr_employee"],
+    //     ["billable_entity"],
+    //     ["bills"],
+    //     ["commission"],
+    //     ["skill"],
+    // ]
+
+    // entities.forEach(e => initCrud(app, e[0], `hr_platform.${e[1] || e[0]}`))
     app.listen(port, () => logger.info(`server listening on port ${port}`));
 })();
+
+process.on('uncaughtException', console.log)
